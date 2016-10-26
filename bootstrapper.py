b7603abd24f61ac051990b7e6d2b1ec728efa80f
Ekuklu, B <ekuklu@icloud.com>
2016-10-26 03:50:51 +0300

    FEATURE(AUTH): Added user authentication table and activation table
    Added bootstrapper script.
    Added users table.
    Added activation table.

---
+++ bootstrapper.py
@@ -0,0 +1,27 @@
+import psycopg2 as db
+
+def init(dsn):
+    with db.connect(dsn) as connection:
+        cursor = connection.cursor()
+
+        cursor.execute("DROP SCHEMA public CASCADE")
+        cursor.execute("CREATE SCHEMA public")
+
+        cursor.execute("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
+
+        cursor.execute("""CREATE TABLE users(
+                            id SERIAL PRIMARY KEY,
+                            username character varying(255) UNIQUE NOT NULL,
+                            password character varying(255) NOT NULL,
+                            email character varying(255) UNIQUE NOT NULL,
+                            activation_key uuid DEFAULT uuid_generate_v4() UNIQUE NOT NULL,
+                            inserted_at timestamp DEFAULT now() NOT NULL
+                      )""")
+
+        cursor.execute("""CREATE TABLE user_activations(
+                            user_id integer PRIMARY KEY,
+                            inserted_at timestamp DEFAULT now() NOT NULL,
+                            FOREIGN KEY ("user_id") REFERENCES users(id)
+                      )""")
+
+    connection.commit()
--- server.py
+++ server.py
@@ -1,17 +1,44 @@
 import datetime
 import os
+import json
+import re
+import psycopg2 as dbapi2
 
 from flask import Flask
 from flask import render_template
 from handlers import www
 
+import bootstrapper
+
 app = Flask(__name__)
 app.register_blueprint(www)
 
+def get_elephantsql_dsn(services_env):
+    # Returns the data source name for ElephantSQL.
+
+    parsed = json.loads(services_env)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+
+    return dsn
+
 if __name__ == '__main__':
-    VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
-    if VCAP_APP_PORT is not None:
-        port, debug = int(VCAP_APP_PORT), False
+    app_port = os.getenv('VCAP_APP_PORT')
+
+    if app_port is not None:
+        port, debug = int(app_port), False
     else:
         port, debug = 5000, True
+
+    services_env = os.getenv('VCAP_SERVICES')
+
+    if services_env is not None:
+        app.config['dsn'] = get_elephantsql_dsn(services_env)
+    else:
+        app.config['dsn'] = """host='localhost' port='5432' dbname='foodle_dev' user='mask' password='qwerty'"""
+
+    bootstrapper.init(app.config['dsn'])
     app.run(host='0.0.0.0', port=port, debug=debug)
