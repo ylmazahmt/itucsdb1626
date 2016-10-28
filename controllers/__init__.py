d588e1a64e61d9a47553332c8b4fa4dfa834755c
Izel Citil <izelcitil@Izel-MacBook-Pro.local>
2016-10-28 21:51:05 +0300

    IMPL(Posts): Implemented Posts API.
    Added templates, model and controller for posts.
    Added SQL seed procedures.

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -4,3 +4,4 @@ from controllers.feed_controller import feed_controller
 from controllers.user_friends_controller import user_friends_controller
 from controllers.application_controller import application_controller
 from controllers.places_controller import places_controller
+from controllers.posts_controller import posts_controller
