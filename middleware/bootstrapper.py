6c494eaf92b5326dc9ec762c40a59933771f690a
Ahmet Yılmaz <ylmazahmt@gmail.com>
2016-10-29 17:43:09 +0300

    INSERT INTO command for user_friends table in init script

--- middleware/bootstrapper.py
+++ middleware/bootstrapper.py
@@ -12,4 +12,8 @@ def __init__():
 
     cursor.execute("INSERT INTO user_activations (user_id) VALUES (%s)", [cursor.fetchone()[0]])
 
+    cursor.execute("INSERT INTO users (username, password_digest, email) VALUES (%s, %s, %s) RETURNING id", ["gugar", bcrypt.hashpw("test".encode('utf-8'), bcrypt.gensalt()).decode('utf-8'), "gugar@mail.com"])
+
+    cursor.execute("INSERT INTO user_friends (user_id, friend_id, is_friend) VALUES (1,2,False)")
+
     connection.commit()
