f586563892996010f19034b0defc7bea2fdd5f61
Ekuklu, B <ekuklu@icloud.com>
2016-10-31 03:31:30 +0200

    IMPL(SQL): Added seeder SQL fileIMPL(SQL): Added seeder SQL file..

--- middleware/bootstrapper.py
+++ middleware/bootstrapper.py
@@ -1,5 +1,6 @@
 from middleware import db
 import bcrypt
+import os
 
 def __init__():
     connection = db.connection
@@ -8,12 +9,15 @@ def __init__():
 
     cursor.execute(open("sql/init.sql", "r").read())
 
-    cursor.execute("INSERT INTO users (username, password_digest, email) VALUES (%s, %s, %s) RETURNING id", ["admin", bcrypt.hashpw("test".encode('utf-8'), bcrypt.gensalt()).decode('utf-8'), "test@mail.com"])
+    cursor.execute("INSERT INTO users (username, password_digest, email) VALUES (%s, %s, %s) RETURNING id", ["admin", bcrypt.hashpw("test".encode('utf-8'), bcrypt.gensalt()).decode('utf-8'), "admin@mail.com"])
 
     cursor.execute("INSERT INTO user_activations (user_id) VALUES (%s)", [cursor.fetchone()[0]])
 
     cursor.execute("INSERT INTO users (username, password_digest, email) VALUES (%s, %s, %s) RETURNING id", ["gugar", bcrypt.hashpw("test".encode('utf-8'), bcrypt.gensalt()).decode('utf-8'), "gugar@mail.com"])
 
     cursor.execute("INSERT INTO user_friends (user_id, friend_id, is_friend) VALUES (1,2,False)")
+
+    if os.environ.get('NO_SEED') is None:
+        cursor.execute(open("sql/seed.sql", "r").read())
 
     connection.commit()
---
+++ sql/seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (username, password_digest, email)
+VALUES ('test', '$2b$12$N/fbLZ21CmeFVH9yHvlZxuqpOTo96698ZKes5jpfGihkNo4M3EEya', 'test@mail.com');
+
+INSERT INTO user_images(user_id, data)
+VALUES (1, '00000');
