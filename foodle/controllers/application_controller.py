eb0fb354535e3aa24c6561a643d877904ee7b0e1
Buğra Ekuklu <ekuklu@icloud.com>
2016-11-23 04:55:35 +0200

    FIX: Redirected index page to Roberts' feed.

--- foodle/controllers/application_controller.py
+++ foodle/controllers/application_controller.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 import foodle
 import psycopg2
-from flask import Blueprint, render_template
-from flask import current_app
+from flask import Blueprint, render_template, redirect
 
 application_controller = Blueprint('application_controller', __name__)
 
 @application_controller.route('/', methods=['GET'])
 def index():
-    return render_template('/index.html')
+    return redirect('/users/2/feed')
